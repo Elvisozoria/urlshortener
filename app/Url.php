4e4a111d1dd9f5a4b25d0bf137ad58cde581ef87
Elvis Ozoria <elvis.ozoria@gmail.com>
2021-03-22 07:13:10 -0400

    logic for encoding, visiting and decoding urls

--- app/Url.php
+++ app/Url.php
@@ -7,4 +7,48 @@ use Illuminate\Database\Eloquent\Model;
 class Url extends Model
 {
     protected $fillable = ['original_url', 'short_url', 'title', 'hits', 'user_id'];
+
+    public function generateShortUrl(){
+        $long_url = $this -> original_url;
+        $this->short_url = $this->encode($this->id);
+        $this->save();
+    }
+
+    
+
+    const ALPHABET = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_';
+	const BASE = 51; // strlen(self::ALPHABET)
+
+	public static function encode($num) {
+		$str = '';
+
+		while ($num > 0) {
+			$str = self::ALPHABET[($num % self::BASE)] . $str;
+			$num = (int) ($num / self::BASE);
+		}
+
+		return $str;
+	}
+
+	//we won't use this for now
+	public static function decode($str) {
+		$num = 0;
+		$len = strlen($str);
+
+		for ($i = 0; $i < $len; $i++) {
+			$num = $num * self::BASE + strpos(self::ALPHABET, $str[$i]);
+		}
+
+		return $num;
+	}
+	
+    public function visit()
+    {
+        $this -> hits += 1;
+        $this -> save();
+        return $this -> original_url;
+
+    }
 }
+   
+
