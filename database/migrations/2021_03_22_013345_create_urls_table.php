92fd67280289a15875b1ca638cc07ff78565747c
Elvis Ozoria <elvis.ozoria@gmail.com>
2021-03-21 21:43:03 -0400

    base for urls

---
+++ database/migrations/2021_03_22_013345_create_urls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('original_url');
+            $table->string('short_url');
+            $table->string('title');
+            $table->integer('hits');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
