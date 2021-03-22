8f92629bae7c1f19b18d181bd8eb1fb1222eed52
Elvis Ozoria <elvis.ozoria@gmail.com>
2021-03-22 07:13:48 -0400

    calling the method inside the url object

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -18,18 +18,31 @@ class UrlController extends Controller
 
     public function store(Request $request)
     {
-        Url::create([
+
+        
+        
+        $url_object = Url::create([
             'original_url' => $request->original_url,
             'short_url' => '',
             'title' => '',
             'hits' => 1,
             'user_id' => 1,
         ]);
+
+        $url_object->generateShortUrl();
+     
         return back();
     }
     public function destroy(Url $url)
     {
         $url->delete();
         return back();
+    }
+
+    public function redirect($shorturl)
+    {
+        $url_object = Url::where('short_url',$shorturl)->first();
+        // dd($url_object);
+        return redirect($url_object->visit());
     }
 }
