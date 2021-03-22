e8ecb5fe15cd98c5cba43a414ae1137aef5312d5
Elvis Ozoria <elvis.ozoria@gmail.com>
2021-03-21 22:48:30 -0400

    creating index,store and destroy functions

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Url;
 use Illuminate\Http\Request;
 
 class UrlController extends Controller
 {
-    //
+    public function index ()
+    {
+        $urls = Url::latest()->get();
+        
+        return view('urls.index',[
+            'urls' => $urls
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        Url::create([
+            'original_url' => $request->original_url,
+            'short_url' => '',
+            'title' => '',
+            'hits' => 1,
+            'user_id' => 1,
+        ]);
+        return back();
+    }
+    public function destroy(Url $url)
+    {
+        $url->delete();
+        return back();
+    }
 }
